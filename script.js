4c47e695a616fca1219c8d01611dd4d3cf70afd2
Shubham <40598861+vijit101@users.noreply.github.com>
2024-11-01 18:35:06 +0530

    Card : updating dynamically using xml request
    updating the card dynamically

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const divEle = document.querySelector('.card-container');
+let data = '';
 
   const req = new XMLHttpRequest();
 
@@ -6,5 +7,17 @@ const divEle = document.querySelector('.card-container');
   req.send();
 
   req.addEventListener('load', function () {
-    console.log(req.responseText);
+    console.log(req.responseText); // string 
+    data = JSON.parse(req.responseText);
+    const cardHtml = `<div class="user-card">
+      <img src= ${data.image} alt="Profile Image" />
+      <h3>${data.firstName}</h3>
+      <h3>${data.lastName}</h3>
+      <p class="email">${data.email}</p>
+      <button class="btn">View Profile</button>
+      </div>`;
+    divEle.innerHTML = cardHtml;
+    
   });
+
+
